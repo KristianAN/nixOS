d8aa5a297818b287536be02202938e633734b159
Kristian Nedrevold-Hansen <kristian@krined.no>
2023-07-16 01:34:10 +0200

    added hw config

---
+++ hosts/laptop/boot.nix
@@ -0,0 +1,5 @@
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.blacklistedKernelModules = [ "elan_i2c" ];
+}
---
+++ hosts/laptop/hardware.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  hardware.opengl.driSupport = true;
+  hardware.keyboard.zsa.enable = true;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # high-resolution display
+  # hardware.video.hidpi.enable = lib.mkDefault true;
+  # Set your system kind (needed for flakes)
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/18f84455-a5b3-4ad7-9b3f-0fea5afdacb0";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/BE22-3181";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/360ed538-beea-484f-ac1c-3e6de5b13842"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
+
+}
