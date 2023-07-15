8b558c840f42e40afd6f369ba7c0dc0fad10f124
Kristian Nedrevold-Hansen <kristian@krined.no>
2023-07-16 00:13:07 +0200

    Fix for touchpad

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nixOS
 My config for nixOS workstation for development
+
+First attempt at anything nix. Gradually moving my entire workflow to use nixOS and nix flakes.
+
+TODOs
+ - [ ] Figure out how to do host spesific settings in a simple pragmatic way. Modularity can come later
+
+
--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.blacklistedKernelModules = [ "elan_i2c" ];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
