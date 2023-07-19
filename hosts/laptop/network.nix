93594d582046e782a09caaba09efedbd4a0b4aef
Kristian Nedrevold-Hansen <kristian@krined.no>
2023-07-20 01:40:26 +0200

    hostname changed

--- hosts/laptop/network.nix
+++ hosts/laptop/network.nix
@@ -7,7 +7,7 @@
 }:
 with lib;
 with builtins; {
-  networking.hostName = "nixos";
+  networking.hostName = "laptop";
   networking.networkmanager.enable = true;
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
