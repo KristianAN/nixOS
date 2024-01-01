094c349ede2e8809baebe9a7393e843f0e8a9f93
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-01-01 22:43:01 +0100

    Updated pubkey for ssh

--- hosts/sky/user.nix
+++ hosts/sky/user.nix
@@ -4,7 +4,7 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC01cvaT1DaPu/3BtuMypNkKH7jflDZ/LKex6/QM3HZaZS1wlvSvoK3yEYAwRZpHCLzGpJ7dk0+bj/UzKsgyatGGeVzGTp7meJLjHgMYJxPw3+Xd25HvaL+7Elhej0x8YOO/QkvgJW/VN3KVhM1Nbd4MfF0dXLi6zHmBHtjLH/haUpb0ueY/4Aa6lNzdR8GNQ6EmKvSDC98KNw9CTrQ7pjrCEJwgGq6GwomXPJ4IhMwRKElm2f8HN3aT+/aVuQ4p9BMyP10emuxSxAPmPy/Ff6ShlnrV7wXJl4RFC9FQa/K2/SUY/pegs9PBlgEMeyfYeZPlYggnCxlAgfI9hAGSvmfWjcwFfbQ6rbpvUZnE2A8VdkpVRx6gelsiH4z8lHUCjoAUdFPcHpQAoK1uc4QSN5s1cOtSM0Yhr23WFKaZvE7yF/0OPCY3KEGsvzCU6Z8IL2iEV76XL9mZIUAzio++ZUCHHJMgKYdD/0n+jJb1t8CbiBLDrmqwXHArcD2dp8Srlc= kristian@rubble"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCoGAVpUUHoognsrZiV0r1jRsNBRF/K0xEKjo7gam7qiahyNXyMok0BMyVifeGpkc/4LmKM14WAsk3f6NIKYy1OIPdzfi4h3M3TXEcCGqtur/8IsWoA8UWzScmdJFRCcjBfjYj08oEFN2iEgvnC2+1Z4hfQ4ZvS+uU/xi8Pp7Mi1hVAJFzxaEnBpN90z5maQfPDrc7nepxDZQ7gsFmsOF6UIBSoWI9CYgrHbRm0bnTKzoDPvK1cfdD9LNjiFsdlwQqyoTKhyyzR5h0Qdrp+zMhp4TFiamQtLeP7j/1QqWJ1I0lnHCyFB+iFyoKPzOTg1dr4U5uFQe+ss9AlSjjnRzPOIclbKd8tMFQa7ROfzSRrctS3aidUkf768MQZRUJAJyyG+LGVfbI3+okWKWF2W+bMQRGmbXEQ0Oq4uEDPNmwzY4vd3xu1sX6UWw74RKKybAq5eXhKM/VAJnv9JEJUgN1Tg+LasOUCXZE/dTlA5JnJYMuNBgKJETFU6S9c2Y8UBKc= kristian@rubble"
       ];
       extraGroups = ["wheel" "networkmanager" "docker" "libvirtd" "video" "audio" "plugdev"];
     };
