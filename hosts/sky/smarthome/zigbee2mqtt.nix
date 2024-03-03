0499bbd1396c2f026da2cdac2c5b1b8e22b90061
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-03-03 13:04:06 +0100

    Fix

--- hosts/sky/smarthome/zigbee2mqtt.nix
+++ hosts/sky/smarthome/zigbee2mqtt.nix
@@ -1,9 +1,7 @@
 { config, pkgs, lib, ... }:
 let 
   # read from json
-<<<<<<< HEAD
   config = lib.builtins.fromJSON (builtins.readFile /home/kristian/.configuration/mqtt.json);
-
 in 
   networking.firewall.allowedTCPPorts = [ 8080 ];
 
@@ -12,17 +10,6 @@ in
     package = pkgs.zigbee2mqtt;
     settings = {
 
-=======
-  config = lib.builtins.fromJSON (builtins.readFile ~/.configuration/mqtt.json);
-in 
-  networking.firewall.allowedTCPPorts = [ 8080 ];
-
-  services.zigbee2mqtt = {
-    enable = true;
-    package = pkgs.zigbee2mqtt;
-    settings = {
-
->>>>>>> 553f8a0fdfb3a1a806495ba296ed7c491c7ea06c
       permit_join = false;
       # serial.port = "/dev/ttyUSB0";
       frontend = true;
