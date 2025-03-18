24af5c374024903bd5a92d92ecb0cf29bfd62fea
Kristian Nedrevold-Hansen <kristian@krined.no>
2025-03-18 21:45:54 +0100

    shit against the wall...

--- hosts/common/cachix/default.nix
+++ hosts/common/cachix/default.nix
@@ -1,13 +1,21 @@
-{ pkgs
-, lib
-, ...
+{
+  pkgs,
+  lib,
+  ...
 }:
 {
-  imports = [
-    ./kristian.nix
-    ./nix-community.nix
+
+  # Include all three caches in substituters
+  nix.settings.substituters = lib.mkAfter [
+    "https://kristian.cachix.org"
+    "https://nix-community.cachix.org"
   ];
-  nix.settings.substituters = lib.mkAfter [ "https://cache.nixos.org/" ];
+
+  # Include all three trusted public keys
+  nix.settings.trusted-public-keys = lib.mkAfter [
+    "kristian.cachix.org-1:I9DC4coD583Us0HFeCW6KiiXbk6uhlWfB2MPxjJiiUI="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 
   environment.systemPackages = [ pkgs.cachix ];
 }
